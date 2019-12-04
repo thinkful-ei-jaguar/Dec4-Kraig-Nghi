1b5e828b726edecdc72c6efa1ae06efc7a14346d
Nghi Tran <nghi.t.tran8@gmail.com>
2019-12-04 12:25:36 -0800

    drill 2

--- FizzBuzzDrill/index.js
+++ FizzBuzzDrill/index.js
@@ -1,1 +1,35 @@
+/* eslint-disable strict */
 // your code here
+function submmission () {
+  // listening for the form to submit
+  $('#number-chooser').on('submit', event => {
+    // Remove default form behavior to refresh page
+    event.preventDefault();
+    // Store the entered number
+    let entry = $('#number-choice').val();
+
+    // Countdown if datatype is a number
+    if (Number(entry)){
+      // Create a the result div in a box
+      for (let i=0; i<=entry; i++){
+        let box = $('.js-results').append($(`<div class="fizz-buzz-item"><span>${i}</span></div>`));
+      }
+    }g
+    // Print fizz if entry is fizz
+    else if (entry === 'fizz'){
+      let box = $('.js-results').append($('<div class=\'fizz-buzz-item fizz\'><span>fizz</span></div>'));
+    }
+    // Print buzz if entry is buzz
+    else if (entry === 'buzz'){
+      let box = $('.js-results').append($('<div class=\'fizz-buzz-item buzz\'><span>buzz</span></div>'));
+    }
+    // Print fizzbuzz if entry is fizzbuzz
+    else if (entry === 'fizzbuzz'){
+      let box = $('.js-results').append($('<div class=\'fizz-buzz-item fizzbuzz\'><span>fizzbuzz</span></div>'));
+    }
+
+
+  });
+}
+
+$(submmission);
